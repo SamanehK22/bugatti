8ebb0e9bfe2e9e125f9f46e578b15fe2965b28de
Samaneh <samanehkiany82@gmail.com>
2025-05-02 23:19:06 +0330

    test

--- main.cpp
+++ main.cpp
@@ -6,143 +6,50 @@
 
 using namespace std;
 
-void printMenu()
-{
-    cout << endl << "========= Vehicle Store Menu =========" << endl;
-    cout << "1. Add Car" << endl;
-    cout << "2. Add Motorcycle" << endl;
-    cout << "3. Add Bicycle" << endl;
-    cout << "4. Show All Items" << endl;
-    cout << "5. Sell Item" << endl;
-    cout << "6. Rent Item" << endl;
-    cout << "7. Return Rented Item" << endl;
-    cout << "8. Test Drive Item" << endl;
-    cout << "0. Exit" << endl;
-    cout << "======================================" << endl;
-    cout << "Choose an option: ";
-}
-
 int main()
 {
     LinkedList store;
-    int choice;
 
-    do {
-        printMenu();
-        cin >> choice;
+    // Add vehicles
+    store.add(new Car("BMW", "M3 GTR", "White and Blue", 2005, 250, 50000));
+    store.add(new Motorcycle("Kawasaki", "Ninja", "Green", 2020, 300, 15000, 100));
+    store.add(new Bicycle("Trek", "Mountain", "Red", 2021, 20, 30));
 
-        switch (choice){
-            case 1:
-            {
-                string brand, model, color;
-                int year, speed;
-                double price;
+    // Display all vehicles
+    cout << "Available vehicles:" << endl;
+    store.displayAll();
 
-                cout << "Enter brand, model, color, year, max speed, price: ";
-                cin >> brand >> model >> color >> year >> speed >> price;
+    // Test drive first vehicle
+    cout << "\nTesting vehicle 0:" << endl;
+    Vehicle* v = store.get(0);
+    if (v) v->drive();
 
-                Vehicle* car = new Car(brand, model, color, year, speed, price);
-                store.add(car);
-                break;
-            }
-            case 2:
-            {
-                string brand, model, color;
-                int year, speed;
-                double price, rentPrice;
-
-                cout << "Enter brand, model, color, year, max speed, sale price, rent price per day: ";
-                cin >> brand >> model >> color >> year >> speed >> price >> rentPrice;
-
-                Vehicle* moto = new Motorcycle(brand, model, color, year, speed, price, rentPrice);
-                store.add(moto);
-                break;
-            }
-            case 3:
-            {
-                string brand, model, color;
-                int year, speed;
-                double rentPrice;
-
-                cout << "Enter brand, model, color, year, max speed, rent price per day: ";
-                cin >> brand >> model >> color >> year >> speed >> rentPrice;
-
-                Vehicle* bike = new Bicycle(brand, model, color, year, speed, rentPrice);
-                store.add(bike);
-                break;
-            }
-            case 4:
-                store.displayAll();
-                break;
-
-            case 5:
-            {
-                int index;
-                cout << "Enter item index to sell: ";
-                cin >> index;
-                Vehicle* v = store.get(index);
-                if (v) {
-                    if (auto* sellable = dynamic_cast<Sellable*>(v)) {
-                        sellable->sell();
-                    } else {
-                        cout << "This item cannot be sold." << endl;
-                    }
-                } else cout << "Invalid index." << endl;
-                break;
-            }
-
-            case 6:
-            {
-                int index, days;
-                cout << "Enter item index to rent and number of days: ";
-                cin >> index >> days;
-                Vehicle* v = store.get(index);
-                if (v) {
-                    if (auto* rentable = dynamic_cast<Rentable*>(v)) {
-                        rentable->rent(days);
-                    } else {
-                        cout << "This item cannot be rented." << endl;
-                    }
-                } else cout << "Invalid index." << endl;
-                break;
-            }
-
-            case 7:
-            {
-                int index;
-                cout << "Enter item index to return: ";
-                cin >> index;
-                Vehicle* v = store.get(index);
-                if (v) {
-                    if (auto* rentable = dynamic_cast<Rentable*>(v)) {
-                        rentable->returnItem();
-                    } else {
-                        cout << "This item cannot be returned." << endl;
-                    }
-                } else cout << "Invalid index." << endl;
-                break;
-            }
-
-            case 8:
-            {
-                int index;
-                cout << "Enter item index to test drive: ";
-                cin >> index;
-                Vehicle* v = store.get(index);
-                if (v) v->drive();
-                else cout << "Invalid index." << endl;
-                break;
-            }
-
-            case 0:
-                cout << "Exiting..." << endl;
-                break;
-
-            default:
-                cout << "Invalid option. Try again." << endl;
+    // Attempt to sell first vehicle
+    cout << "\nAttempting to sell vehicle 0:" << endl;
+    v = store.get(0);
+    if(v)
+    {
+        if(auto* sellable = dynamic_cast<Sellable*>(v))
+        {
+            sellable->sell();
+        }else
+        {
+            cout << "This vehicle cannot be sold." << endl;
         }
+    }
 
-    } while (choice != 0);
+    // Attempt to rent second vehicle
+    cout << "\nAttempting to rent vehicle 1:" << endl;
+    v = store.get(1);
+    if (v) {
+        if(auto* rentable = dynamic_cast<Rentable*>(v))
+        {
+            rentable->rent(5);
+        }else
+        {
+            cout << "This vehicle cannot be rented." << endl;
+        }
+    }
 
     return 0;
 }
